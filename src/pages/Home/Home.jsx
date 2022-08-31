209db5aaf0b1e48509b94690e026558d3809f8ff
Le Huu Hung <huuhung1962001@gmail.com>
2022-08-31 17:23:26 +0700

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,37 +23,61 @@ function Home() {
   const indexOfFirstMarket = indexOfLastMarket - marketPerPage;
   const currentMarket = list.slice(indexOfFirstMarket, indexOfLastMarket);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const result = [
+    ...list.reduce((set, { category }) => {
+      set.add(category);
+      return set;
+    }, new Set()),
+  ];
   return (
-    <div>
-      <label className="m-5 text-primary text-bold h2">SẢN PHẨM</label>
-      <div className="list_product">
-        <Row xs={1} md={3} className="g-4">
-          {currentMarket.map((market) => (
-            <Col>
-              <Card
-                style={{ width: "18rem", border: "none", cursor: "pointer" }}
-                onClick={() => navigate(`/product/${market.id}`)}
-              > 
-                <Card.Img
-                  variant="top"
-                  src={market.image}
-                  className="img_product"
-                />
-                <Card.Body>
-                  <Card.Title>{market.title}</Card.Title>
-                  <Card.Text>{market.price} $</Card.Text>
-                </Card.Body>
-              </Card>
-              
-            </Col>
-          ))}
-        </Row>
+    <div className="row">
+      <div className="col-2">
+        <label className="  text-primary text-bold h2">&nbsp; SẢN PHẨM</label>
+        <label style={{ fontSize: 20 }}>
+          &nbsp;&nbsp;&nbsp;Loại sản phẩm
+        </label>
+        <br/>
+        {result.map((category) => (
+          <div>
+            &nbsp; &nbsp; &nbsp;
+            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+            <label for="vehicle1" style={{ textTransform: "capitalize" }}>
+              {" "}
+              &nbsp;{category}
+            </label>
+            <br />
+          </div>
+        ))}
       </div>
-      <PaginationCm
-        marketPerPage={marketPerPage}
-        totalMarkets={list.length}
-        paginate={paginate}
-      />
+      <div className="col-10">
+        <div className="list_product">
+          <Row xs={1} md={3} className="g-4">
+            {currentMarket.map((market) => (
+              <Col>
+                <Card
+                  style={{ width: "18rem", border: "none", cursor: "pointer" }}
+                  onClick={() => navigate(`/product/${market.id}`)}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={market.image}
+                    className="img_product"
+                  />
+                  <Card.Body>
+                    <Card.Title>{market.title}</Card.Title>
+                    <Card.Text>{market.price} $</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+        <PaginationCm
+          marketPerPage={marketPerPage}
+          totalMarkets={list.length}
+          paginate={paginate}
+        />
+      </div>
     </div>
   );
 }
